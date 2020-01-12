a4960c85f1dbbb32b08a8d50af7c24f7df5cc59f
Kieron Scott <kieronscott@kierons-mbp-2.home>
2020-01-12 22:20:11 +0000

    Update - Opening hours and remove unused form markup from contact page

--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -2,7 +2,7 @@ document.documentElement.className = "js";
 
 var today = new Date();
 if (today.getDay() == 6 || today.getDay() == 0) {
-  document.getElementById("openingTimes").innerHTML = "09:00 - 17:30";
+  document.getElementById("openingTimes").innerHTML = "09:00 - 17:00";
 } else {
-  document.getElementById("openingTimes").innerHTML = "09:00 - 17:30";
+  document.getElementById("openingTimes").innerHTML = "09:00 - 17:00";
 }
